62a7e56d0b0bfe6ac4500c382b3070acd63a4c15
mohammed1981994 <110145@uotechnology.edu.iq>
2024-04-09 03:40:05 +0300

    Update Topology.py

--- Topology.py
+++ Topology.py
@@ -7,30 +7,38 @@ import os
 
 def OS3EGraph():
     g = nx.Graph()
-    g.add_path(["Vancouver", "Seattle"])
-    g.add_path(["Seattle", "Missoula", "Minneapolis", "Chicago"])
-    g.add_path(["Seattle", "SaltLakeCity"])
-    g.add_path(["Seattle", "Portland", "Sunnyvale"])
-    g.add_path(["Sunnyvale", "SaltLakeCity"])
-    g.add_path(["Sunnyvale", "LosAngeles"])
-    g.add_path(["LosAngeles", "SaltLakeCity"])
-    g.add_path(["LosAngeles", "Tucson", "ElPaso"])
-    g.add_path(["SaltLakeCity", "Denver"])
-    g.add_path(["Denver", "Albuquerque", "ElPaso"])
-    g.add_path(["Denver", "KansasCity", "Chicago"])
-    g.add_path(["KansasCity", "Dallas", "Houston"])
-    g.add_path(["ElPaso", "Houston"])
-    g.add_path(["Houston", "Jackson", "Memphis", "Nashville"])
-    g.add_path(["Houston", "BatonRouge", "Jacksonville"])
-    g.add_path(["Chicago", "Indianapolis", "Louisville", "Nashville"])
-    g.add_path(["Nashville", "Atlanta"])
-    g.add_path(["Atlanta", "Jacksonville"])
-    g.add_path(["Jacksonville", "Miami"])
-    g.add_path(["Chicago", "Cleveland"])
-    g.add_path(["Cleveland", "Buffalo", "Boston", "NewYork", "Philadelphia", "Washington"])
-    g.add_path(["Cleveland", "Pittsburgh", "Ashburn", "Washington"])
-    g.add_path(["Washington", "Raleigh", "Atlanta"])
+    paths = [
+        ["Vancouver", "Seattle"],
+        ["Seattle", "Missoula", "Minneapolis", "Chicago"],
+        ["Seattle", "SaltLakeCity"],
+        ["Seattle", "Portland", "Sunnyvale"],
+        ["Sunnyvale", "SaltLakeCity"],
+        ["Sunnyvale", "LosAngeles"],
+        ["LosAngeles", "SaltLakeCity"],
+        ["LosAngeles", "Tucson", "ElPaso"],
+        ["SaltLakeCity", "Denver"],
+        ["Denver", "Albuquerque", "ElPaso"],
+        ["Denver", "KansasCity", "Chicago"],
+        ["KansasCity", "Dallas", "Houston"],
+        ["ElPaso", "Houston"],
+        ["Houston", "Jackson", "Memphis", "Nashville"],
+        ["Houston", "BatonRouge", "Jacksonville"],
+        ["Chicago", "Indianapolis", "Louisville", "Nashville"],
+        ["Nashville", "Atlanta"],
+        ["Atlanta", "Jacksonville"],
+        ["Jacksonville", "Miami"],
+        ["Chicago", "Cleveland"],
+        ["Cleveland", "Buffalo", "Boston", "NewYork", "Philadelphia", "Washington"],
+        ["Cleveland", "Pittsburgh", "Ashburn", "Washington"],
+        ["Washington", "Raleigh", "Atlanta"]
+    ]
+    
+    for path in paths:
+        for i in range(len(path) - 1):
+            g.add_edge(path[i], path[i+1])
+    
     return g
+
 
 
 def write_json_file(filename, data):
